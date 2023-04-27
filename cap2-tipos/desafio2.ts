6ec718019dd750f711c71caab88235ba14193de8
LcsMonteiro <larimonteirodev@gmail.com>
2023-04-27 15:04:11 -0300

    fix: corrige desafio 2

--- cap2-tipos/desafio2.ts
+++ cap2-tipos/desafio2.ts
@@ -1,15 +1,7 @@
 // Desafio
+// Transforme o código abaixo em Typescript
 
-type ContaBancaria = {
-  saldo: number;
-  depositar: (valor: number) => number;
-};
-
-function saldoConta(saldo: number, valor: number): number {
-  return saldo + valor;
-}
-
-let contaBancaria = {
+/* let contaBancaria = {
   saldo: 3456,
   depositar(valor) {
     this.saldo += valor;
@@ -17,6 +9,35 @@ let contaBancaria = {
 };
 
 let correntista = {
+  nome: "Ana Silva",
+  contaBancaria: contaBancaria,
+  contatos: ["34567890", "98765432"],
+};
+
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
+*/
+// Código em Typescript
+
+interface ContaBancaria {
+  saldo: number;
+  depositar(valor: number): void;
+}
+
+interface Correntista {
+  nome: string;
+  contaBancaria: ContaBancaria;
+  contatos: string[];
+}
+
+let contaBancaria: ContaBancaria = {
+  saldo: 3456,
+  depositar(valor) {
+    this.saldo += valor;
+  },
+};
+
+let correntista: Correntista = {
   nome: "Ana Silva",
   contaBancaria: contaBancaria,
   contatos: ["34567890", "98765432"],
